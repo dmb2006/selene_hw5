012d12d31d0270ac7512d5b7a607ef79201f5661
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-06-01 13:02:40 +0300

    added test filling form

--- conftest/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selene import browser
-
-@pytest.fixture()
-def test_browser_option():
-    browser.config.base_url='https://demoqa.com/automation-practice-form'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1020
