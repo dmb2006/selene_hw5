19fd9f541d047554a7f12fb47d779b0634e07c16
Aleksandr Minin <dmb2006minin@yandex.ru>
2025-06-02 10:47:22 +0300

    change path image

--- test/test_filling_form.py
+++ test/test_filling_form.py
@@ -1,13 +1,14 @@
 from selene import browser, command, have
-import pytest
 import os
+import pytest
 
-file_path = r"D:\home_work_QaGuru\selene_hw5"
+
 
 def test_felling_form(browser_option, credentials):
-    assert os.path.exists(file_path)
 
     browser.open('/automation-practice-form')
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('footer').remove()")
     browser.element('#firstName').type(credentials["first_name"])
     browser.element('#lastName').type(credentials["last_name"])
     browser.element('#userEmail').type(credentials['email'])
@@ -20,15 +21,15 @@ def test_felling_form(browser_option, credentials):
     browser.element('#subjectsInput').type(credentials['subject'])
     browser.element('[for="hobbies-checkbox-2"]').click()
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').send_keys(file_path)
-    browser.element('#currentAddress').perform(command.js.scroll_into_view).type(credentials['current_address'])
-    # browser.element('#currentAddress').type(credentials['current_address'])
+    browser.element('#uploadPicture').send_keys(os.path.abspath('trooper.jpg'))
+    # browser.element('#currentAddress').perform(command.js.scroll_into_view).type(credentials['current_address'])
+    browser.element('#currentAddress').type(credentials['current_address'])
     browser.element('#state').click().element('#react-select-3-option-2').click()
     browser.element('#city').click().element('#react-select-4-option-0').click()
     browser.element('#submit').click()
 
     browser.element('.table-responsive').all('td').even.should(have.exact_texts(
         'Oleg Petrov', 'test@test.ru', 'Male', '7911888664', '12 September,1989', '', 'Reading, Sports',
-        'selene_hw5', 'Saint-Petersburg', 'Haryana Karnal'
+        'trooper.jpg', 'Saint-Petersburg', 'Haryana Karnal'
     ))
 
